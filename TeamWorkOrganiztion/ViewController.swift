0479ad680d7af686fb894e83c6e2606b47db6c7e
Yaroslav Trach <zhbr282@gmail.com>
2023-11-07 12:03:52 -0500

    Initial Commit

---
+++ TeamWorkOrganiztion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamWorkOrganiztion
+//
+//  Created by User on 2023-11-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
